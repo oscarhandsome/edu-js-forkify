ab5885bb13dd3a479fd7f061da01dd12a29b6099
oscarhandsome <altair5555@gmail.com>
2021-03-12 00:31:25 +0600

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e952e042-4e1a-42eb-abee-816d1fa66ea9';
+export const MODAL_CLOSE_SEC = 2.5;
